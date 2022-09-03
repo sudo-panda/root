d633bff19c87967ba15a38e6b984aeb86e47347e
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-09-03 15:21:22 +0200

    [PyROOT] Add types for numba support

---
+++ bindings/pyroot/cppyy/cppyy/python/cppyy/types.py
@@ -0,0 +1,35 @@
+""" C++ proxy types.
+"""
+
+import cppyy
+
+bck = cppyy._backend
+Instance      = bck.CPPInstance
+
+try:
+    import __pypy__
+    __all__ = [
+        'Instance'
+    ]
+
+except ImportError:
+    __all__ = [
+        'DataMember',
+        'Instance',
+        'Function',
+        'Method',
+        'Scope',
+        'InstanceArray',
+        # 'LowLevelView',
+        'Template'
+    ]
+
+    DataMember      = bck.CPPDataMember
+    Function        = bck.CPPOverload
+    Method          = bck.CPPOverload
+    Scope           = bck.CPPScope
+    InstanceArray   = bck.InstancesArray
+    # LowLevelView    = bck.LowLevelView
+    Template        = bck.TemplateProxy
+
+del bck
