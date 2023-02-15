693fee4c75283fa54ff0057711f1fb8e08e92854
Baidyanath Kundu <kundubaidya99@gmail.com>
2023-02-15 16:31:53 +0100

    check if measures are working

--- bindings/pyroot/pythonizations/python/ROOT_numba/__init__.py
+++ bindings/pyroot/pythonizations/python/ROOT_numba/__init__.py
@@ -1,1 +1,11 @@
+import sys
+
+if sys.version_info < (3, 7):
+    raise Exception("ROOT numba extension requires Python 3.7 or higher")
+
+import numba
+
+if not hasattr(numba, 'version_info') or numba.version_info < (0, 54):
+    raise Exception("ROOT numba extension requires Numba version 0.54 or higher")
+
 import cppyy.numba_ext
